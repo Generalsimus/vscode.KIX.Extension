18c506c4012ebc7e51e268999aab338305771628
Generalsimus <totorotomoney@gmail.com>
2023-04-04 21:05:02 +0400

    fix proxy file redirect

--- client/src/host/index.ts
+++ client/src/host/index.ts
@@ -8,13 +8,13 @@ import { uriToString } from './utils/uriToString';
 import { CreateContentAreaController } from '../utils/createContentAreaController';
 import { createPerfHook } from '../perf';
 
-const proxyRangeUpdate = <T extends Record<any, any>, A extends T | T[]>(
+const proxyRedirectFile = <T extends Record<any, any>, A extends T | T[]>(
 	target: A,
 	positionDetails: ReturnType<TextDocumentController["getDocumentUpdateDocumentContentAtPositions"]>,
 	originalUri: Uri
 ): A => {
 	if (Array.isArray(target)) {
-		return target.map(el => proxyRangeUpdate(el, positionDetails, originalUri)) as A;
+		return target.map(el => proxyRedirectFile(el, positionDetails, originalUri)) as A;
 	}
 
 	if (typeof target !== "object") {
@@ -23,31 +23,42 @@ const proxyRangeUpdate = <T extends Record<any, any>, A extends T | T[]>(
 	const state: Partial<T> = {};
 	return new Proxy(target, {
 		get(obj, prop, receiver) {
+			console.log("🚀 --> file: index.ts:26 --> get --> prop:", obj, prop);
 			if (prop in state) {
 				return Reflect.get(state, prop);
 			}
 			const value = Reflect.get(obj, prop, receiver);
 
+			if (value instanceof Uri) {
+				console.log("🚀 --> Uri:", uriToString(positionDetails.uri), uriToString(value), uriToString(positionDetails.uri) === uriToString(value));
+
+				Reflect.set(state, prop, originalUri);
+				return originalUri;
+			}
+
 			if (value instanceof Range) {
 				const updatedRange = positionDetails.areaController.updateRange(value);
 
+				console.log("🚀 --> file: index.ts:44 --> get --> Range:");
 				Reflect.set(state, prop, updatedRange);
 
 				return updatedRange;
 			} else if (value instanceof Position) {
 				const updatedPosition = positionDetails.areaController.updatePosition(value);
 
+				console.log("🚀 --> file: index.ts:44 --> get --> Position:");
 				Reflect.set(state, prop, updatedPosition);
 
 				return updatedPosition;
-			} else if (value instanceof Uri && uriToString(positionDetails.uri) === uriToString(originalUri)) {
+			} else if (value instanceof Uri && uriToString(positionDetails.uri) === uriToString(value)) {
+				console.log("🚀 --> file: index.ts:44 --> get --> Uri:");
 
 				Reflect.set(state, prop, originalUri);
 				return originalUri;
 			}
 
 			if (typeof value === "object") {
-				const proxyObject = proxyRangeUpdate(value, positionDetails, originalUri);
+				const proxyObject = proxyRedirectFile(value, positionDetails, originalUri);
 				Reflect.set(state, prop, proxyObject);
 				return proxyObject;
 			}
@@ -55,7 +66,7 @@ const proxyRangeUpdate = <T extends Record<any, any>, A extends T | T[]>(
 			return value;
 		},
 		set(obj, prop, value, receiver) {
-			if (value instanceof Position || value instanceof Uri || value instanceof Range) {
+			if (value instanceof Position || value instanceof Uri || value instanceof Range || prop in state) {
 				return Reflect.set(state, prop, value);
 			}
 			return Reflect.set(obj, prop, value, receiver);
@@ -83,6 +94,7 @@ export class TextDocumentController {
 			ts.ScriptKind.KTS
 		);
 	}
+	generatedFileEndList: string[] = []
 	getDocumentUpdateDocumentContentAtPositions(position: Position) {
 		const offset = this.textdocument.offsetAt(position);
 		const styleContentNode = findContentLocationNode(offset, this.sourceFile.kixStyleTagChildNodes);
@@ -124,62 +136,42 @@ export class TextDocumentController {
 				positionDetails.position,
 				triggerCharacter
 			).then((completionList) => {
-				return proxyRangeUpdate(completionList, positionDetails, uri);
+				return proxyRedirectFile(completionList, positionDetails, uri);
 			});
 		}
-		
+
 		return commands.executeCommand<CompletionList>(
 			'vscode.executeCompletionItemProvider',
 			positionDetails.uri,
 			positionDetails.position,
 			triggerCharacter
 		).then((completionList) => {
-			const comp = proxyRangeUpdate(completionList, positionDetails, uri);
+			const comp = proxyRedirectFile(completionList, positionDetails, uri);
 			// console.log("🚀 --> file: index.ts:137 --> TextDocumentController --> ).then --> comp:", comp.items.map(e => ({ ...e })));
-			// return comp;
-			const { areaController } = positionDetails;
-			return {
-				items: completionList.items.map(item => {
-					const { range } = item;
-					if (range) {
-						if (range instanceof Range) {
-							item.range = areaController.updateRange(range);
-						} else {
-							const { inserting, replacing } = range;
-							range.inserting = areaController.updateRange(inserting);
-							range.replacing = areaController.updateRange(replacing);
-						}
-					}
-					return item;
-				}),
-				isIncomplete: completionList.isIncomplete
-			};
+			return comp;
+			// const { areaController } = positionDetails;
+			// return {
+			// 	items: completionList.items.map(item => {
+			// 		const { range } = item;
+			// 		if (range) {
+			// 			if (range instanceof Range) {
+			// 				item.range = areaController.updateRange(range);
+			// 			} else {
+			// 				const { inserting, replacing } = range;
+			// 				range.inserting = areaController.updateRange(inserting);
+			// 				range.replacing = areaController.updateRange(replacing);
+			// 			}
+			// 		}
+			// 		return item;
+			// 	}),
+			// 	isIncomplete: completionList.isIncomplete
+			// };
 		});
 
 	}
-	provideHover(position: Position) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
-		const Hover = commands.executeCommand<Hover[]>(
-			'vscode.executeHoverProvider',
-			positionDetails.uri,
-			positionDetails.position
-		);
-		// console.log("🚀 --> file: index.ts:98 --> TextDocumentController --> provideHover --> Hover:", Hover);
-		return Hover[0];
-	}
-	provideSignatureHelp(position: Position, triggerCharacter: string | undefined) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
-		const SignatureHelp = commands.executeCommand<SignatureHelp>(
-			"vscode.executeSignatureHelpProvider",
-			positionDetails.uri,
-			positionDetails.position,
-			triggerCharacter
-		);
-		// console.log("🚀 --> file: index.ts:112 --> TextDocumentController --> provideSignatureHelp --> SignatureHelp:", SignatureHelp);
-		return SignatureHelp;
-	}
 	provideDefinition(position: Position, uri: Uri) {
 		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+		// console.log("🚀 --> file: index.ts:183 --> TextDocumentController --> provideDefinition --> positionDetails:", positionDetails.textContent);
 		// const { areaController } = positionDetails;
 		// console.log("🚀 --> file: index.ts:118 --> TextDocumentController --> provideDefinition --> positionDetails:", positionDetails);
 
@@ -199,84 +191,107 @@ export class TextDocumentController {
 			positionDetails.uri,
 			positionDetails.position,
 		).then(definitions => {
-			return proxyRangeUpdate(definitions, positionDetails, uri);
+			console.log("🚀 --> file: index.ts:186 --> TextDocumentController --> provideDefinition --> definitions:", definitions);
+			return proxyRedirectFile(definitions, positionDetails, uri);
 		});
 	}
-	provideTypeDefinition(position: Position) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
-		const typeDefinition = commands.executeCommand<Definition | DefinitionLink[]>(
-			'vscode.executeTypeDefinitionProvider',
-			positionDetails.uri,
-			positionDetails.position,
-		);
-		// console.log("🚀 --> file: index.ts:122 --> TextDocumentController --> provideDefinition --> definition:", typeDefinition);
-		// რეინჯი განაახლე აქქქ
-		return typeDefinition;
-	}
-	provideImplementation(position: Position) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
-		const implementation = commands.executeCommand<Definition | DefinitionLink[]>(
-			'vscode.executeTypeDefinitionProvider',
-			positionDetails.uri,
-			positionDetails.position,
-		);
-		// console.log("🚀 --> file: index.ts:145 --> TextDocumentController --> provideImplementation --> implementation:", implementation);
-		// რეინჯი განაახლე აქქქ
-		return implementation;
-	}
-	provideReferences(position: Position) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
-		const references = commands.executeCommand<Location[]>(
-			'vscode.executeReferenceProvider',
-			positionDetails.uri,
-			positionDetails.position,
-		);
-		// console.log("🚀 --> file: index.ts:155 --> TextDocumentController --> provideReferences --> references:", references);
+	// provideHover(position: Position) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+	// 	const Hover = commands.executeCommand<Hover[]>(
+	// 		'vscode.executeHoverProvider',
+	// 		positionDetails.uri,
+	// 		positionDetails.position
+	// 	);
+	// 	// console.log("🚀 --> file: index.ts:98 --> TextDocumentController --> provideHover --> Hover:", Hover);
+	// 	return Hover[0];
+	// }
+	// provideSignatureHelp(position: Position, triggerCharacter: string | undefined) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+	// 	const SignatureHelp = commands.executeCommand<SignatureHelp>(
+	// 		"vscode.executeSignatureHelpProvider",
+	// 		positionDetails.uri,
+	// 		positionDetails.position,
+	// 		triggerCharacter
+	// 	);
+	// 	// console.log("🚀 --> file: index.ts:112 --> TextDocumentController --> provideSignatureHelp --> SignatureHelp:", SignatureHelp);
+	// 	return SignatureHelp;
+	// }
 
-		// რეინჯი განაახლე აქქქ
-		return references;
-	}
-	provideDocumentHighlights(position: Position) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
-		const documentHighlights = commands.executeCommand<DocumentHighlight[]>(
-			'vscode.executeDocumentHighlights',
-			positionDetails.uri,
-			positionDetails.position,
-		);
-		// console.log("🚀 --> file: index.ts:167 --> TextDocumentController --> provideDocumentHighlights --> documentHighlights:", documentHighlights);
+	// provideTypeDefinition(position: Position) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+	// 	const typeDefinition = commands.executeCommand<Definition | DefinitionLink[]>(
+	// 		'vscode.executeTypeDefinitionProvider',
+	// 		positionDetails.uri,
+	// 		positionDetails.position,
+	// 	);
+	// 	// console.log("🚀 --> file: index.ts:122 --> TextDocumentController --> provideDefinition --> definition:", typeDefinition);
+	// 	// რეინჯი განაახლე აქქქ
+	// 	return typeDefinition;
+	// }
+	// provideImplementation(position: Position) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+	// 	const implementation = commands.executeCommand<Definition | DefinitionLink[]>(
+	// 		'vscode.executeTypeDefinitionProvider',
+	// 		positionDetails.uri,
+	// 		positionDetails.position,
+	// 	);
+	// 	// console.log("🚀 --> file: index.ts:145 --> TextDocumentController --> provideImplementation --> implementation:", implementation);
+	// 	// რეინჯი განაახლე აქქქ
+	// 	return implementation;
+	// }
+	// provideReferences(position: Position) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+	// 	const references = commands.executeCommand<Location[]>(
+	// 		'vscode.executeReferenceProvider',
+	// 		positionDetails.uri,
+	// 		positionDetails.position,
+	// 	);
+	// 	// console.log("🚀 --> file: index.ts:155 --> TextDocumentController --> provideReferences --> references:", references);
 
-		// რეინჯი განაახლე აქქქ
-		return documentHighlights;
-	}
-	provideDocumentSymbols(position: Position) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
-		const documentSymbols = commands.executeCommand<SymbolInformation[] | DocumentSymbol[]>(
-			'vscode.executeDocumentSymbolProvider',
-			positionDetails.uri,
-		);
-		// console.log("🚀 --> file: index.ts:178 --> TextDocumentController --> provideDocumentSymbols --> documentSymbols:", documentSymbols);
+	// 	// რეინჯი განაახლე აქქქ
+	// 	return references;
+	// }
+	// provideDocumentHighlights(position: Position) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+	// 	const documentHighlights = commands.executeCommand<DocumentHighlight[]>(
+	// 		'vscode.executeDocumentHighlights',
+	// 		positionDetails.uri,
+	// 		positionDetails.position,
+	// 	);
+	// 	// console.log("🚀 --> file: index.ts:167 --> TextDocumentController --> provideDocumentHighlights --> documentHighlights:", documentHighlights);
 
-		// რეინჯი განაახლე აქქქ
-		return documentSymbols;
-	}
-	provideCodeActions(range: Range) {
-		const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(range.start);
-		if (positionDetails.isStyle === false) {
-			range = positionDetails.areaController.updateRange(range);
-		}
-		const codeActions = commands.executeCommand<(Command | CodeAction)[]>(
-			'vscode.executeCodeActionProvider',
-			positionDetails.uri,
-			range
-		);
+	// 	// რეინჯი განაახლე აქქქ
+	// 	return documentHighlights;
+	// }
+	// provideDocumentSymbols(position: Position) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(position);
+	// 	const documentSymbols = commands.executeCommand<SymbolInformation[] | DocumentSymbol[]>(
+	// 		'vscode.executeDocumentSymbolProvider',
+	// 		positionDetails.uri,
+	// 	);
+	// 	// console.log("🚀 --> file: index.ts:178 --> TextDocumentController --> provideDocumentSymbols --> documentSymbols:", documentSymbols);
 
-		return codeActions;
-		// const result = await base.provideCodeActions?.(document, range, context, token);
-		// console.log("provideCodeActions");
-		// const textDocumentController = getTextDocumentController(document);
-		// 
-		// return textDocumentController.provideCodeActions(position);
-		//
-		// return result?.filter(codeAction => codeAction.title.indexOf('__VLS_') === -1);
-	}
+	// 	// რეინჯი განაახლე აქქქ
+	// 	return documentSymbols;
+	// }
+	// provideCodeActions(range: Range) {
+	// 	const positionDetails = this.getDocumentUpdateDocumentContentAtPositions(range.start);
+	// 	if (positionDetails.isStyle === false) {
+	// 		range = positionDetails.areaController.updateRange(range);
+	// 	}
+	// 	const codeActions = commands.executeCommand<(Command | CodeAction)[]>(
+	// 		'vscode.executeCodeActionProvider',
+	// 		positionDetails.uri,
+	// 		range
+	// 	);
+
+	// 	return codeActions;
+	// 	// const result = await base.provideCodeActions?.(document, range, context, token);
+	// 	// console.log("provideCodeActions");
+	// 	// const textDocumentController = getTextDocumentController(document);
+	// 	// 
+	// 	// return textDocumentController.provideCodeActions(position);
+	// 	//
+	// 	// return result?.filter(codeAction => codeAction.title.indexOf('__VLS_') === -1);
+	// }
 }
